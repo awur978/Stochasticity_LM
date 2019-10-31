d360558b0ced931a1b0663cf589d7b467961c531
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2019-11-01 09:50:22 +1300

    Update apply.m

--- +elliotsigR/apply.m
+++ +elliotsigR/apply.m
@@ -1,7 +1,4 @@
 function a = apply(n,param)
-%ELLIOTSIG.APPLY Apply transfer function to inputs
-
-% Copyright 2012-2015 The MathWorks, Inc.
 	R = 0.1;
     rn = R * (rand(size(n))*2-1) .* n/1; 
 a = n ./ (1 + abs(n))  + rn ;
